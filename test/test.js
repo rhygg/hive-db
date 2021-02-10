3b4380cce39cbb5617633429b5f7c771009e2ec2
Rhydderchc <rhydderchc@gmail.com>
2021-02-10 15:59:31 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ const db = hive.sqlite;
 db.init('foo', 'bar');
 console.log(db.get('foo'));
 const {database} = hive.mongo;
-const mongo = new database("mongodb+srv://wyvern:coolbossbitch@cluster0.67lsz.mongodb.net", "JSON", { useUnique: true });
+const mongo = new database("mongodb+srv://wyvern:thebestbot@cluster0.67lsz.mongodb.net", "JSON", { useUnique: true });
 
 mongo.on("ready", () => {
     console.log(`Database has been connected!`);
